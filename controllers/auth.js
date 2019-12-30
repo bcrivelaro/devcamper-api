a4f3aef0e8479c8ab6fff53ac84bf80ac0c2a205
Bruno Crivelaro <brunosantoscrivelaro@gmail.com>
2019-12-30 10:49:28 -0300

    implement login endpoint

--- controllers/auth.js
+++ controllers/auth.js
@@ -21,3 +21,35 @@ exports.register = asyncHandler(async (req, res, next) => {
 
   res.status(200).json({ success: true, token });
 });
+
+// @desc      Login user
+// @route     POST /api/v1/auth/login
+// @access    Public
+exports.login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // Validates email and password
+  if (!email || !password) {
+    return next(
+      new ErrorResponse('Please provide a valid email and password', 400)
+    );
+  }
+
+  // Check for user
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user) {
+    return next(new ErrorResponse('Invalid credentials', 401));
+  }
+
+  // Match password
+  const isMatch = await user.matchPassword(password);
+
+  if (!isMatch) {
+    return next(new ErrorResponse('Invalid credentials', 401));
+  }
+
+  const token = user.getSignedJwtToken();
+
+  res.status(200).json({ success: true, token });
+});
